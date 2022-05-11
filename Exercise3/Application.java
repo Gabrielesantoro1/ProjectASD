3797bf75602cf065dbdd163598c4dc0d2f8b3bd9
Gabriele Santoro <gabriele.santoro956@edu.unito.it>
2022-05-11 16:01:11 +0200

    Junit file created + application file created

---
+++ Exercise3/Application.java
@@ -0,0 +1,14 @@
+package Exercise3;
+
+
+/**
+ * Application class where it s possible to try our library MinHeap
+ * 
+ */
+public class Application {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Exercise3/MinHeapTests.java
@@ -0,0 +1,18 @@
+package Exercise3;
+
+
+
+/**
+ * To use the test class:
+ * 1)Compile the class where we have our methods to test:
+ * javac nomeClasse.java
+ * 
+ * 2)Compile the test:
+ * javac -cp .;junit-4.XX.jar;hamcrest-core-1.3.jar MinHeapTests.java
+ *  
+ * 3)Run the test:
+ * java -cp .;junit-4.XX.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore MinHeapTests.java
+ */
+public class MinHeapTests {
+    
+}
