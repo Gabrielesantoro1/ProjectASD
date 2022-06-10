140b1912abe06bde8f45b8c893e81540a9393b59
Carmine Santella <carmine.santella@edu.unito.it>
2022-06-10 16:26:11 +0200

    Applicative for Exercise 3

--- Exercise3/Application.java
+++ Exercise3/Application.java
@@ -2,6 +2,7 @@ package Exercise3;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import Exercise3.src.ComparatorInteger;
 import Exercise3.src.ComparatorString;
@@ -16,97 +17,170 @@ public class Application {
         /**
          * MinHeap for Integer data
          */
-        ComparatorInteger compInt = new ComparatorInteger();
-        Random r = new Random();
-
-        ArrayList<Integer> array = new ArrayList<>();
-        for(int k = 0; k<135; k++){
-          array.add(r.nextInt(200)+1);    
-        }
-
-        System.out.println("Before build min heap:");
-        System.out.println(array.toString());
-        System.out.println("Array size:" + array.size());
-        System.out.println("\n");
-
-        try{
-        MinHeap<Integer> heap = new MinHeap<>(compInt);
-        heap.setArray(array);
-
-        int x = -1;
-        System.out.println("After insert new element:"+x);
-        heap.minHeapInsert(x);
-        System.out.println(heap.toString());
-        System.out.println("Heap size:"+heap.getHeapSize());
-
-        System.out.println("\n");
-
-        System.out.println("After decreased element in index:"+1);
-        heap.heapDecreaseKey(1, -1);
-        System.out.println(heap.toString());
-        System.out.println("Heap size:"+heap.getHeapSize());
-
-        System.out.println("\n");
-
-        System.out.println("Extraction of the min value of the heap:");
-        System.out.println(heap.heapExtractMin());
-        System.out.println(heap.toString());
-        System.out.println("Heap size:"+heap.getHeapSize());
-
-        }catch(MinHeapException e){
-          e.printStackTrace();
-        }
-        System.out.println("");
-
-        /**
-         * MinHeap for String data 
-         */
-        ComparatorString compStr = new ComparatorString();
-        ArrayList<String> array_str = new ArrayList<>();
-        array_str.add("AAABC");
-        array_str.add("SDFAD");
-        array_str.add("asdasd");
-        array_str.add("adhfdbs");
-        array_str.add("arrrra");
-
-        System.out.println("Before build min heap:");
-        System.out.println(array_str.toString());
-        System.out.println("Array size:"+array_str.size());
-
-        System.out.println("");
-        try{
-        MinHeap<String> heap_str = new MinHeap<>(compStr);
-        heap_str.setArray(array_str);
-        heap_str.buildMinHeap();
-        System.out.println("After build min heap:");
-        System.out.println(heap_str.toString());
-        System.out.println("Heap size:"+heap_str.getHeapSize());
-        
-        System.out.println("");
-
-        String s = "z";
-        System.out.println("After insert new element:"+s);
-        heap_str.minHeapInsert(s);
-        System.out.println(heap_str.toString());
-        System.out.println("Heap size:"+heap_str.getHeapSize());
-
-        System.out.println("\n");
-
-        System.out.println("After decreased element in index:"+1);
-        heap_str.heapDecreaseKey(1, "z");
-        System.out.println(heap_str.toString());
-        System.out.println("Heap size:"+heap_str.getHeapSize());
-
-        System.out.println("\n");
-
-        System.out.println("Extraction of the min value of the heap:");
-        System.out.println(heap_str.heapExtractMin());
-        System.out.println(heap_str.toString());
-        System.out.println("Heap size:"+heap_str.getHeapSize());
-        
-        }catch(MinHeapException e){
-          e.printStackTrace();
-        }
-        System.out.println("");
+          ComparatorInteger compInt = new ComparatorInteger();
+          Random r = new Random();
+          Scanner scanner = new Scanner(System.in);
+  
+          try{
+            ArrayList<Integer> array = new ArrayList<>();
+            MinHeap<Integer> heap = new MinHeap<>(compInt);
+            heap.setArray(array);
+            Boolean choice = true;
+  
+            System.out.println("Let's build a minHeap! How many random elements you want the program to insert? ");
+            Integer elem = scanner.nextInt();
+              choice = false;
+          
+            for(int k = 0; k<elem; k++){
+            int x = (r.nextInt(200)+1);
+              heap.minHeapInsert(x);   
+            }
+  
+            System.out.println("Do you want to print your minHeap? y/n");
+            String print = scanner.nextLine();
+            if(print.equals("y"))
+              System.out.println(heap.toString());
+  
+            while(choice){
+              System.out.println("What function of the heap would you like to try? The possible choices are:" );
+              System.out.println("in for insert; ex for extractMin; dec for DecreaseKey; getp for getFather; getl for GetLeft; getr for GetRight ");
+              System.out.println("If you want to exit write exit");
+              String fun = scanner.nextLine();
+  
+              switch(fun){
+                case "in":
+                System.out.println("Write the element to insert: ");
+                Integer j = scanner.nextInt();
+                heap.minHeapInsert(j);
+                System.out.println("Now the heap is "+heap.toString());
+                break;
+  
+                case "ex":
+                System.out.println("You choose extractmin! The element extracted is: "+heap.heapExtractMin());
+                System.out.println("Heap now : "+heap.toString());
+                break;
+  
+                case "dec":
+                System.out.println("What's the index of the element you want to decrease?");
+                Integer index = scanner.nextInt();
+                System.out.println("Now insert the element you wnat to put at that index");
+                Integer key = scanner.nextInt();
+                heap.heapDecreaseKey(index, key);
+                System.out.println("Heap actually : "+heap.toString());
+                break;
+  
+                case "getp":
+                System.out.println("insert the index of the element whose father you want to get: ");
+                Integer k = scanner.nextInt();
+                System.out.println(heap.getParentElem(array, k));
+                break;
+  
+                case "getl":
+                System.out.println("insert the index of the element whose left son you want to get: ");
+                Integer m = scanner.nextInt();
+                System.out.println(heap.getLeftElem(array, m));
+                break;
+  
+                case "getr":
+                System.out.println("insert the index of the element whose lright son you want to get: ");
+                Integer n = scanner.nextInt();
+                System.out.println(heap.getRightElem(array, n));
+                break;
+  
+                case "exit":
+                  choice = false;
+                  break;
+  
+              }
+  
+            }
+            
+          }catch(MinHeapException e){
+            e.printStackTrace();
+          }
+          System.out.println("");
+  
+          /**
+           * MinHeap for String data 
+           */
+          ComparatorString compStr = new ComparatorString();
+          ArrayList<String> array_str = new ArrayList<>();
+          Boolean choice = true;
+            try{
+              System.out.println("Do you want to build a heap with Strings? y/n");
+              choice = scanner.nextLine().equals("y") ? true : false;
+              if(choice){
+                MinHeap<String> heap_str = new MinHeap<>(compStr);
+                heap_str.setArray(array_str);
+  
+                System.out.println("First of all, we have to insert some elements; how many?");
+                Integer x = scanner.nextInt();
+  
+                for(int j=0;j<x;j++){
+                  String s = scanner.next();
+                  heap_str.minHeapInsert(s);
+                }
+  
+                System.out.println("Let's print the heap : "+heap_str.toString());
+                while(choice){
+                  System.out.println("Now what function of the heap would you like to try? The possible choices are:" );
+                  System.out.println("in for insert; ex for extractMin; dec for DecreaseKey; getp for getFather; getl for GetLeft; getr for GetRight ");
+                  System.out.println("If you want to exit write exit");
+                  String fun = scanner.nextLine();
+  
+                  switch(fun){
+                    case "in":
+                    System.out.println("Write the element to insert: ");
+                    String j = scanner.nextLine();
+                    heap_str.minHeapInsert(j);
+                    System.out.println("Now the heap is "+heap_str.toString());
+                    break;
+  
+                    case "ex":
+                    System.out.println("You choose extractmin! The element extracted is: "+heap_str.heapExtractMin());
+                    System.out.println("Heap now : "+heap_str.toString());
+                    break;
+  
+                    case "dec":
+                    System.out.println("What's the index of the element you want to decrease?");
+                    Integer index = scanner.nextInt();
+                    System.out.println("Now insert the element you wnat to put at that index");
+                    String key = scanner.nextLine();
+                    heap_str.heapDecreaseKey(index, key);
+                    System.out.println("Heap actually : "+heap_str.toString());
+                    break;
+  
+                    case "getp":
+                    System.out.println("insert the index of the element whose father you want to get: ");
+                    Integer k = scanner.nextInt();
+                    System.out.println(heap_str.getParentElem(array_str, k));
+                    break;
+  
+                    case "getl":
+                    System.out.println("insert the index of the element whose left son you want to get: ");
+                    Integer m = scanner.nextInt();
+                    System.out.println(heap_str.getLeftElem(array_str, m));
+                    break;
+  
+                    case "getr":
+                    System.out.println("insert the index of the element whose lright son you want to get: ");
+                    Integer n = scanner.nextInt();
+                    System.out.println(heap_str.getRightElem(array_str, n));
+                    break;
+  
+                    case "exit":
+                      choice = false;
+                      break;
+                  }
+                }
+            }else{
+              System.out.println("Ok, let's exit");
+            }
+              
+            }catch(MinHeapException e){
+              e.printStackTrace();
+            }
+          scanner.close();
+          System.out.println("");
     }
 }
